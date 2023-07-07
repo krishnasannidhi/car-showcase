46ccc2b2ce4863b092e613089333d09dc2502f64
krishnasannidhi <krishna.sannidhi@gmail.com>
2023-07-07 08:19:38 -0500

    added images in public and home page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,6 +20,12 @@ const Hero = () => {
           handleClick={handleScroll}
         ></CustomButton>
       </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
+        </div>
+      </div>
     </div>
   );
 };
