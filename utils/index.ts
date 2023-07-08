1bd9802fd12740694201665da515097b9ed768b1
krishnasannidhi <krishna.sannidhi@gmail.com>
2023-07-08 09:17:18 -0500

    updated

--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,5 @@
 export async function fetchCars() {
-  const headers = {
-    "X-RapidAPI-Key": "5ef66f4ce7msha390eae9da771cdp16697ajsn68da87e63e11",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-  };
+  const headers = {};
 
   const response = await fetch(
     "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
