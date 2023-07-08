e7b5dce31074ad6b0a1c7a53369cde9c31923957
krishnasannidhi <krishna.sannidhi@gmail.com>
2023-07-08 13:56:18 -0500

    added car details component

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { CarProps } from "@/types";
 import { calculateCarRent } from "@/utils";
 import CustomButton from "./CustomButton";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -67,6 +68,11 @@ const CarCard = ({ car }: CarCardProps) => {
           ></CustomButton>
         </div>
       </div>
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
--- components/index.ts
+++ components/index.ts
@@ -6,6 +6,7 @@ import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManfacturer from "./SearchManfacturer";
 import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
 export {
   Hero,
@@ -16,4 +17,5 @@ export {
   CustomFilter,
   SearchManfacturer,
   CarCard,
+  CarDetails,
 };
